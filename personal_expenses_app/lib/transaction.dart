552bb49fdb6b2e3c88fbbab46078255963c06c8f
MinhSu <minhsu12113@gmai.com>
2022-06-04 17:14:51 +0700

    Add new file Traction

---
+++ personal_expenses_app/lib/transaction.dart
@@ -0,0 +1,8 @@
+class Transaction {
+ final String? id;
+ final String? title;
+ final double? amount;
+ final DateTime? dateTime;
+
+ Transaction({required this.id, required this.title, required this.amount, required this.dateTime});
+}
